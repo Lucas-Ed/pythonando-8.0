30e4d46abee99e3cf23ab75cc83a22e60efe211a
Lucas-Ed <lucas_tranpo@yahoo.com.br>
2023-10-27 17:58:09 -0300

    Atualização

---
+++ vitalab/build_files.sh
@@ -0,0 +1,6 @@
+pip install -r requirements.txt 
+python manage.py collectstatic
+
+python mangege.py makemigrations
+
+python mangege.py migrate
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-s=!lr4mhc^fy!g(=(*2=7q3$kn)4)u*gee&(!$t2-5s($y5=gv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -80,10 +81,20 @@ CSRF_TRUSTED_ORIGINS = ['https://pythonando83-1v2rzgj2.b4a.run']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 

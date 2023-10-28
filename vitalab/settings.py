712c366cae805b7802409f01127b6bbca06b1774
Lucas eduardo ⚡ <lucas_tranpo@YAHOO.com.br>
2023-10-28 18:59:54 +0000

    Correção

--- vitalab/settings.py
+++ vitalab/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s=!lr4mhc^fy!g(=(*2=7q3$kn)4)u*gee&(!$t2-5s($y5=gv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

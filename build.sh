86e614d191856b86c297548d6dffa14762ee1cf5
Lucas eduardo ⚡ <lucas_tranpo@YAHOO.com.br>
2023-10-28 01:56:28 +0000

    Atualização

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -9,9 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dotenv 
 from pathlib import Path
 import os
+
+# lê as variáveis de ambiente
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s=!lr4mhc^fy!g(=(*2=7q3$kn)4)u*gee&(!$t2-5s($y5=gv'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+
+
+
 #ALLOWED_HOSTS = ['127.0.0.1','localhost', 'https://pythonando-8-0.onrender.com']
 
 
@@ -74,20 +75,27 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vitalab.wsgi.application'
 
-PYTHON_VERSION = '3.11.4'
 
-CSRF_TRUSTED_ORIGINS = ['https://pythonando-8-0.onrender.com']
+
+
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#}
+import dj-database-url
+import os
+
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
+	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

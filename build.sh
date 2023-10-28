2d6eb0b2566159b5a8085c3379248595dbbcf7a0
Lucas eduardo ⚡ <lucas_tranpo@YAHOO.com.br>
2023-10-28 02:29:17 +0000

    Atualização

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 
-python -m pip install -r requirements.txt
-python -m pip install --force-reinstall -U setuptools
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations

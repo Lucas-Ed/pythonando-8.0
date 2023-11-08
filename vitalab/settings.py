2d296475039e920701ed093335702502e581c32d
Lucas-Ed <lucas_tranpo@yahoo.com.br>
2023-11-08 12:16:45 -0300

    Atualização

--- vitalab/settings.py
+++ vitalab/settings.py
@@ -26,12 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-#ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 
